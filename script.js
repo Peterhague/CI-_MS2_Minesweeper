b3ba0eea4612ca7e7d2b87d5bfa2c21230f91f2a
Peterhague <prhague@hotmail.co.uk>
2021-07-26 12:41:56 +0000

    added more code to the function to generate grid rows

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ let gridRow = ""; //these will be the rows (divs) to be appended to the mineswee
 let difficulty = document.getElementById("difficulty"); //targets the dropdown selector per the html file
 function generatedGridRows() {
     let selectedDifficulty = difficulty.value; //being the selected difficulty option in the dropdown
-    
+    if (selectedDifficulty == "Easy") {
+        for (i = 0; i < 9; i++) {
+            gridRow = document.createElement("div");
+            gridRow.classList.add("grid-rows", `grid-row-${i}`) // to be able to identify the row precisely if necessary
+            grid.appendChild(gridRow);
+        } // this loop creates 9 rows and adds them to the grid - EASY mode
+    }
 }
