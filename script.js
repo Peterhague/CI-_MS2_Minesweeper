01d326f08628b727f25a02f867a200236fcf75cc
Peterhague <prhague@hotmail.co.uk>
2021-07-26 12:30:28 +0000

    created first js code

---
+++ script.js
@@ -0,0 +1,3 @@
+let randomSquares = []; //creates the array to be populated by the randomise function
+let grid = document.getElementById("grid");
+let gridRow = ""; //these will be the rows (divs) to be appended to the minesweeper grid
