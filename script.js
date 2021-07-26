6dccef5113fd6e83e08ab09f73fd8c44b91153ae
Peterhague <prhague@hotmail.co.uk>
2021-07-26 12:35:57 +0000

    started the function to generate grid rows

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let randomSquares = []; //creates the array to be populated by the randomise function
 let grid = document.getElementById("grid");
 let gridRow = ""; //these will be the rows (divs) to be appended to the minesweeper grid
+let difficulty = document.getElementById("difficulty"); //targets the dropdown selector per the html file
+function generatedGridRows() {
+    let selectedDifficulty = difficulty.value; //being the selected difficulty option in the dropdown
+    
+}
