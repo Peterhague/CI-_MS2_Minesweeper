0cc29a6079366bf469e466dd823250204830f87b
Peterhague <prhague@hotmail.co.uk>
2021-07-28 21:25:25 +0000

    made some minor changes

--- script.js
+++ script.js
@@ -366,71 +366,71 @@ ALSO have a value of zero. */
     automatedClick(this);
 }
 function minesweepRight(squares, that) {
-  let thisID = that.id;
-  squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + (selectedRows -1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - (selectedRows +1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
+    let thisID = that.id;
+    squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + (selectedRows -1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - (selectedRows +1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
 }
 function minesweepLeft(squares, that) {
-  let thisID = that.id;
-  squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - (selectedRows -1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + (selectedRows +1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
+    let thisID = that.id;
+    squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - (selectedRows -1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + (selectedRows +1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
 }
 function minesweepTopLeft(squares, that) {
-  let thisID = that.id;
-  squares[parseInt(thisID) + (selectedRows +1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
+    let thisID = that.id;
+    squares[parseInt(thisID) + (selectedRows +1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
 }
 function minesweepTop(squares, that) {
-  let thisID = that.id;
-  squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + (selectedRows -1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + (selectedRows +1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
+    let thisID = that.id;
+    squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + (selectedRows -1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + (selectedRows +1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
 }
 function minesweepTopRight(squares, that) {
-  let thisID = that.id;
-  squares[parseInt(thisID) + (selectedRows -1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
+    let thisID = that.id;
+    squares[parseInt(thisID) + (selectedRows -1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
 }
 function minesweepBottom(squares, that) {
-  let thisID = that.id;
-  squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - (selectedRows -1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - (selectedRows +1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
+    let thisID = that.id;
+    squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - (selectedRows -1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - (selectedRows +1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
 }
 function minesweepBottomLeft(squares, that) {
-  let thisID = that.id;
-  squares[parseInt(thisID) -selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - (selectedRows -1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
+    let thisID = that.id;
+    squares[parseInt(thisID) -selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - (selectedRows -1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
 }
 function minesweepBottomRight(squares, that) {
-  let thisID = that.id;
-  squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - (selectedRows +1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
+    let thisID = that.id;
+    squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - (selectedRows +1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
 }
 function minesweepMiddle(squares, that) {
-  let thisID = that.id;
-  squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - (selectedRows -1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - (selectedRows +1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + (selectedRows -1)].classList.add("clicked-square-radius");
-  squares[parseInt(thisID) + (selectedRows +1)].classList.add("clicked-square-radius");
+    let thisID = that.id;
+    squares[parseInt(thisID) - selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - (selectedRows -1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - (selectedRows +1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + selectedRows].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + 1].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) - 1].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + (selectedRows -1)].classList.add("clicked-square-radius");
+    squares[parseInt(thisID) + (selectedRows +1)].classList.add("clicked-square-radius");
 }
 function automatedClick(that) {
     let clickedSquareRadius = document.getElementsByClassName("clicked-square-radius");
