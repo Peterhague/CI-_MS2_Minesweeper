b474c87dab1e00a7cc68bc2ebec4121841b4f9fa
Peterhague <prhague@hotmail.co.uk>
2021-07-29 00:33:41 +0000

    added iniital styling for header

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let titleContainer = document.getElementById("title-container");
+
 let randomSquares = []; //creates the array to be populated by the randomise function
 let grid = document.getElementById("grid");
 let gridRow = ""; //these will be the rows (divs) to be appended to the minesweeper grid
