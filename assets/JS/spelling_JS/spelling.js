38e2843d5e5dde142d6a59b7bcf2901d0328e3fd
Peterhague <prhague@hotmail.co.uk>
2021-08-02 10:28:24 +0000

    improved questionCounter fn to iterate over array

--- assets/JS/spelling_JS/spelling.js
+++ assets/JS/spelling_JS/spelling.js
@@ -1,43 +1,42 @@
 let playButton = document.getElementById("play-button");
 playButton.addEventListener("click", clearAnswer);// clears the answer input after each submission
-playButton.addEventListener("click", generateQuestions);// generates an array of 10 random words from the dictionary as the quiz questions
+/*playButton.addEventListener("click", generateQuestions);*//// generates an array of 10 random words from the dictionary as the quiz questions
 let difficultySpelling = document.getElementById("difficulty-spelling");
 let submit = document.getElementById("submit");
 submit.addEventListener("click", spellcheck);// checks the given user input is the same as the dictionary spelling
 submit.addEventListener("click", questionCounter);
-submit.onclick = function() {questionCounter(randomWords)};
 let inputTwo = document.getElementById("input-two");
 let output = "";
-let currentQuestion;
-let randomWords = [];
+let currentQuestion = 0;
+let randomWords = generateQuestions();
 let answerBox = document.getElementById("answer-box");
 function generateQuestions() {
-  randomWords = [];
-  while (randomWords.length < 10) {
+  x = [];
+  while (x.length < 10) {
     if (difficultySpelling.value == "Easy") {
       let word = dictionary[0][Math.floor(Math.random()*200)];
-      if (randomWords.includes(word) === false) {
-        randomWords.push(word);
+      if (x.includes(word) === false) {
+        x.push(word);
       }
     } else if (difficultySpelling.value == "Medium") {
       let word = dictionary[1][Math.floor(Math.random()*200)];
-      if (randomWords.includes(word) === false) {
-        randomWords.push(word);
+      if (x.includes(word) === false) {
+        x.push(word);
       }
     } else if (difficultySpelling.value == "Hard") {
       let word = dictionary[2][Math.floor(Math.random()*200)];
-      if (randomWords.includes(word) === false) {
-        randomWords.push(word);
+      if (x.includes(word) === false) {
+        x.push(word);
       }
     } else if (difficultySpelling.value == "Very Hard") {
       let word = dictionary[3][Math.floor(Math.random()*200)];
-      if (randomWords.includes(word) === false) {
-        randomWords.push(word);
+      if (x.includes(word) === false) {
+        x.push(word);
       }
     }
   }
-  speak(randomWords, 0);
-  console.log(randomWords);
+  speak(x, 0);
+  return x;
 }
 function speak(randomWords, iteration) {
   responsiveVoice.speak(randomWords[iteration]);
@@ -53,7 +52,11 @@ function clearAnswer() {
   inputTwo.value = "";
   answerBox.innerHTML = "";
 }
-function questionCounter(randomWords) {
+function questionCounter() {
   currentQuestion += 1;
+  console.log(currentQuestion);
   speak(randomWords, currentQuestion);
+  console.log(randomWords[1]);
 }
+
+console.log(randomWords);
