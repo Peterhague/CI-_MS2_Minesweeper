373f7b9abdce93740bf35f70466235de3646edfd
Peterhague <prhague@hotmail.co.uk>
2021-08-06 10:44:58 +0000

    got basic emailJS function working

--- assets/JS/homepage_JS/homepage.js
+++ assets/JS/homepage_JS/homepage.js
@@ -1,13 +1,16 @@
-var templateParams = {
-    name: 'James',
-    notes: 'Check this out!'
-};
-function sendMail(templateParams) {
-    console.log("worked");
-    emailjs.send('default_service', 'contact_form', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+function sendMail(contactForm) {
+    emailjs.send("service_epz8iti", "contact_form", {
+        "to_name": contactForm.toName.value,
+        "fron_name": contactForm.fromName.value,
+        "from_email": contactForm.fromEmail.value,
+        "message": contactForm.message.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+    return false;
 }
