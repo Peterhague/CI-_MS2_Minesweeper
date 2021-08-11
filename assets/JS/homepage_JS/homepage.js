8f7c065ad0e6ce656102fd25defb4c342233be8f
Peterhague <prhague@hotmail.co.uk>
2021-08-11 13:38:41 +0000

    added form validation via JS and automatic click of anchor tag to scroll page to error message location

--- assets/JS/homepage_JS/homepage.js
+++ assets/JS/homepage_JS/homepage.js
@@ -5,5 +5,40 @@ function sendMail(contactForm) {
         "email_address": contactForm.emailAddress.value,
         "message": contactForm.suggestion.value,
     })
+    console.log("pass thru working");
     return false;
+    
+}
+function formValidation(that) {
+    let fName = document.forms["ideasForm"]["firstName"].value;
+    let lName = document.forms["ideasForm"]["lastName"].value;
+    let email = document.forms["ideasForm"]["emailAddress"].value;
+    let errormsg = document.getElementById("formErrorMessage");
+    let showMessage = document.getElementById("navigateToFooter");
+    if (fName == "") {
+        errormsg.innerHTML = "**Please enter your first name**";
+        errormsg.classList.add("text-red");
+        errormsg.classList.remove("text-green");
+        showMessage.click();
+        return false;
+    } else if (lName == "") {        
+        errormsg.innerHTML = "**Please enter your last name**";
+        errormsg.classList.add("text-red");
+        errormsg.classList.remove("text-green");
+        showMessage.click();
+        return false;
+    } else if (email == "") {
+        errormsg.innerHTML = "**Please enter your email address**";
+        errormsg.classList.add("text-red");
+        errormsg.classList.remove("text-green");
+        showMessage.click();
+        return false;
+    } else {
+        sendMail(that);
+        errormsg.innerHTML = `Thanks for getting in touch!<br>We'll get back to you soon <i class="far fa-smile"></i>`
+        errormsg.classList.remove("text-red");
+        errormsg.classList.add("text-green");
+        showMessage.click();
+        return false;
+    }
 }
