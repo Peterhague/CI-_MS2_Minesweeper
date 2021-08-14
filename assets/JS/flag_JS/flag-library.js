68046223b3f1480f49a52b9eb14397298ca382f7
Peterhague <prhague@hotmail.co.uk>
2021-08-14 01:07:28 +0000

    deleted unnecessary flags array

--- assets/JS/flag_JS/flag-library.js
+++ assets/JS/flag_JS/flag-library.js
@@ -1,13 +1,3 @@
-/*let flags = ['ad','ae','af','ag','ai','al','am','ao','ar','at','au','az','ba','bb','bd','be','bf','bg',
-'bh','bi','bj','bo','br','bs','bt','bw','by','bz','ca','cd','cf','cg','ch','ci','cl','cm','cn','co','cr','cu',
-'cv','cy','cz','de','dj','dk','dm','do','dz','ec','ee','eg','eh','er','es','et','fi','fj','fm','fr','ga','gb',
-'gd','ge','gf','gh','gm','gn','gq','gr','gt','gw','gy','hn','hr','ht','hu','id','ie','il','in','iq','ir','is',
-'it','jm','jo','jp','ke','kg','kh','ki','km','kn','kp','kr','kw','kz','la','lb','lc','li','lk','lr','ls','lt',
-'lu','lv','ly','ma','md','me','mg','mk','ml','mm','mn','mr','mt','mu','mv','mw','mx','my','mz','na','ne','ng',
-'ni','nl','no','np','nr','nz','om','pa','pe','pg','ph','pk','pl','ps','pt','pw','py','qa','ro','rs','ru','rw',
-'sa','sc','sd','se','sg','si','sk','sl','sm','sn','so','sr','ss','st','sv','sy','sz','td','tg','th','tj','tl',
-'tm','tn','to','tr','tt','tv','tz','ua','ug','us','uy','uz','va','vc','ve','vn','vu','ye','za','zm','zw']*/
-
 let flagObjects = [{'code': 'ad', 'name': 'Andorra'},{'code': 'ae', 'name': 'United Arab Emirates', 'altNames': 'UAE'},
 {'code': 'af', 'name': 'Afghanistan'},{'code': 'ag', 'name': 'Antigua and Barbuda'},{'code': 'ai', 'name': 'Anguilla'},
 {'code': 'al', 'name': 'Albania'},{'code': 'am', 'name': 'Armenia'},{'code': 'ao', 'name': 'Angola'},
