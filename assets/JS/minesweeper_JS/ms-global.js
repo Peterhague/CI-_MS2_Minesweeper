1145a2ee6ce5550fcb8d2bb9754205c95f2c2122
Peterhague <prhague@hotmail.co.uk>
2021-08-14 02:21:11 +0000

    finished commenting on and tidying up ms-global js file

--- assets/JS/minesweeper_JS/ms-global.js
+++ assets/JS/minesweeper_JS/ms-global.js
@@ -22,7 +22,6 @@ function addPlayFunctions() {
     play.addEventListener("click", assignHTML);
     play.addEventListener("click", bombIcon);
     play.addEventListener("click", flags);
-    play.addEventListener("click", setFocus);
 }
 //removes all event listeners from the play button, except stylingChanges
 function removePlayFunctions() {
@@ -36,7 +35,6 @@ function removePlayFunctions() {
     play.removeEventListener("click", assignHTML);
     play.removeEventListener("click", bombIcon);
     play.removeEventListener("click", flags);
-    play.removeEventListener("click", setFocus);
 }
 addPlayFunctions();
 //event handler for change in difficulty. Ajusts the number of rows/squares etc to be used in various other functions
@@ -114,7 +112,9 @@ function randomise() {
 /* the for y loop iterates through the rows of the grid and its parent x loop generates the same number of
 squares for each row, to end up with a square grid of 'squares'.
 Each square is assigned a unique id based on its place in the grid, ie its row number (y) multiplied by the
-total number of rows, plus its position in the row (x).*/
+total number of rows, plus its position in the row (x).
+If this number is included in the randomSquares list of 'mine' squares, then the square is assigned the class
+of "bomb", otherwise the class of "no-bomb".*/
 function newGame() {
     for (x = 0; x < selectedRows; x++) {
         for (y = 0; y < selectedRows; y++) { 
@@ -133,6 +133,7 @@ function newGame() {
         }   
     }        
 }
+//assigns squares different classes based on the selected difficulty, which will then determine their size for better UI
 function squareSize() {
     let squares = document.getElementsByClassName("squares");
     let selectedDifficulty = difficulty.value;
@@ -146,22 +147,23 @@ function squareSize() {
         }
     }
 }
+//adds the various event listeners to the individual squares
 function squaresFunctions(squares) {
-    squares.addEventListener("click", minesweep); // function to detect how many mines are in the squares surrounding the clicked square
+    squares.addEventListener("click", minesweep); 
     squares.addEventListener("touchstart", longPressDown);
     squares.addEventListener("touchend", longPressUp);
-    squares.addEventListener("click", counter); // function counts how many squares with no mines have been clicked to determine ultimate success
-    squares.addEventListener("click", gameOverOne); // performs part of the game over "animation" sequence
-    squares.addEventListener("click", gameOverTwo); // performs part of the game over "animation" sequence
+    squares.addEventListener("click", counter);
+    squares.addEventListener("click", gameOverOne);
+    squares.addEventListener("click", gameOverTwo);
 }
+/*the outer loop iterates over the rows of the grid and the nested loops iterates over the 
+squares (ie children of the rows). The code says if the row's index is even then the first square
+of that row is given the class of "odd-squares", and every alternate square of that row is
+given the class of "even-squares", and then the reverse for rows with indices that are odd.
+This is is purely for styling in the checkerboard pattern, and to distinguish the code applied to 
+every alternate row of the grid. If the same code was applied to every row the grid would have
+vertical stripes and not a chequered effect. */
 function checkerboard() {
-/* the outer loop iterates over the rows of the grid and the nested loop iterates over the 
-    squares (ie children of the rows). The code says if the row's index is even then the first square
-    of that row is given the class of "odd-squares", and every alternate square of that row is
-    given the class of "even-squares", and then the reverse for rows with indices that are odd.
-    This is purely for styling in the checkerboard pattern, and to distinguish the code applied to 
-    every alternate row of the grid. If the same code was applied to every row the grid would have
-    vertical stripes and not a chequered effect. */
     for (let i = 0; i < selectedRows; i++) {
         for (let x = 0; x < selectedRows; x++) {
             if (i % 2 === 0 && x % 2 === 0) {
@@ -179,20 +181,17 @@ function checkerboard() {
         }
     }
 }
+// simple loop to assign squares with class of "bomb" the skull font awesome icon as innerHTML
 function bombIcon() {
     let squares = document.getElementsByClassName("squares");  
-    for (square of squares) { // simple loop to assign squares with class of "bomb" the skull font awesome icon as innerHTML
+    for (square of squares) { 
         if (square.classList.contains("bomb")) {
             square.innerHTML = `<i class="fas fa-skull"></i>`;
         }
     }
 }
+// Assigns the starting value of the number flags in hand counter depending on the selected difficulty
 function flags() {
-    /* Assigns the starting value of the number flags in hand counter depending on the selected difficulty*/
     scoreContainer.innerHTML = "";
     scoreContainer.innerHTML = selectedBombs;
-}
-function setFocus() {
-    let minesweeperBottom = document.getElementById("minesweeperBottom");
-    minesweeperBottom.focus();
 }
