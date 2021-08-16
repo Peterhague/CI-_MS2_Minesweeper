817084fe761c3b5c3e5cafa07e052f544126451c
Peterhague <prhague@hotmail.co.uk>
2021-08-16 02:35:39 +0000

    changed width of flags title pixels to 0.95vw on mobile

--- assets/JS/title_JS/title-flags.js
+++ assets/JS/title_JS/title-flags.js
@@ -8,7 +8,7 @@ for (i = 0; i < 10; i++) {
     titleRows.id = `title-row${i}`;
     for (x = 0; x < 100; x++) {
         let titlePixels = document.createElement("div");
-        titlePixels.classList.add("title-pixels");
+        titlePixels.classList.add("title-pixels-flags");
         titlePixels.style.backgroundColor = "white";
         titlePixels.id = "pixel" + ((i*100)+x);
         titleRows.appendChild(titlePixels);
@@ -39,7 +39,7 @@ let titlePixelsColored = ["pixel500","pixel600","pixel700","pixel701","pixel601"
 "pixel692","pixel792","pixel793","pixel693","pixel593","pixel493","pixel393","pixel794","pixel893","pixel894","pixel895","pixel896","pixel897",
 "pixel898","pixel798","pixel698","pixel699","pixel799","pixel899","pixel597","pixel598","pixel599","pixel293","pixel294","pixel194","pixel195",
 "pixel196","pixel197","pixel198","pixel199","pixel299", "pixel142","pixel837","pixel829","pixel134"]
-let titlePixels = document.getElementsByClassName("title-pixels");
+let titlePixels = document.getElementsByClassName("title-pixels-flags");
 /*assigns background color to the title divs based on their position to spell out a title. The divs in the titlePixelsColored
 array are the ones spelling out the words. Additionally, certain divs are coloured differently in order to create the effect
 of mini tricolour flags occupying parts of the title*/
