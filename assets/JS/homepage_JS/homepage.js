4bf0f93f2d6776416873300277404aaece96d7ce
Peterhague <prhague@hotmail.co.uk>
2021-08-20 21:36:38 +0000

    fixed bug in email JS code, needed conditional statement for mbolie/desktop form distinction

--- assets/JS/homepage_JS/homepage.js
+++ assets/JS/homepage_JS/homepage.js
@@ -2,8 +2,8 @@
 (function() {
     emailjs.init("user_TnGv5LfOGP5Nqwj1AQR0U");
   })();
-/*passes the emailJS API the necessary inputs to send the automated email*/
-function sendMail(contactForm) {
+/*passes the emailJS API the necessary inputs to send the automated email when in mobile configuration*/
+function sendMailMobile(contactForm) {
     emailjs.send("service_epz8iti", "contact_form", {
         "first_name": contactForm.firstName.value,
         "last_name": contactForm.lastName.value,
@@ -12,61 +12,129 @@ function sendMail(contactForm) {
     })
     return false;    
 }
+/*passes the emailJS API the necessary inputs to send the automated email when in desktop configuration*/
+function sendMailDesktop(contactForm) {
+    emailjs.send("service_epz8iti", "contact_form", {
+        "first_name": contactForm.firstNameDesktop.value,
+        "last_name": contactForm.lastNameDesktop.value,
+        "email_address": contactForm.emailAddressDesktop.value,
+        "message": contactForm.suggestionDesktop.value,
+    })
+    return false;    
+}
 /*function to validate the form for user idea submissions.
+Checks if mobile or desktop form should be addressed, then:
 Checks that all fields are populated.
 Checks that the name fields contain only letters.
+Checks that the email address field is populated and is in a valid format
+Checks that the message textarea is populated
 If all the conditions are met then the sendMail function is called and an email is sent via emailJS.
 @param that [user suggestions form]
 */
 function formValidation(that) {
-    let fName = document.forms["ideasForm"]["firstName"].value;
-    let lName = document.forms["ideasForm"]["lastName"].value;
-    let email = document.forms["ideasForm"]["emailAddress"].value;
-    let suggestion = document.forms["ideasForm"]["suggestion"].value;
-    let errormsg = document.getElementById("formErrorMessage");
-    let showMessage = document.getElementById("navigateToFooter");
-    let letters = /^[A-Za-z]+$/;
-    let emailContent = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (fName == "") {
-        errormsg.innerHTML = "**Please enter your first name**";
-        errormsg.classList.add("text-red");
-        errormsg.classList.remove("text-green");
-        showMessage.click();
-        return false;
-    } else if (lName == "") {        
-        errormsg.innerHTML = "**Please enter your last name**";
-        errormsg.classList.add("text-red");
-        errormsg.classList.remove("text-green");
-        showMessage.click();
-        return false;
-    } else if (email == "") {
-        errormsg.innerHTML = "**Please enter your email address**";
-        errormsg.classList.add("text-red");
-        errormsg.classList.remove("text-green");
-        showMessage.click();
-        return false;
-    } else if (suggestion == "") {
-        errormsg.innerHTML = "**Please enter your message**";
-        errormsg.classList.add("text-red");
-        errormsg.classList.remove("text-green");
-        showMessage.click();
-        return false;
-    } else if (!fName.match(letters) || !lName.match(letters)) {        
-        errormsg.innerHTML = "**The name fields should only contain characters A-Z**";
-        showMessage.click();
-        return false;
-    }  else if (email.match(emailContent)) {
-        sendMail(that);
-        errormsg.innerHTML = `Thanks for getting in touch!<br>We'll get back to you soon <i class="far fa-smile"></i>`
-        errormsg.classList.remove("text-red");
-        errormsg.classList.add("text-green");
-        showMessage.click();
-        return false;
-    }  else {
-        errormsg.innerHTML = "**Please enter a valid email address**";
-        errormsg.classList.add("text-red");
-        errormsg.classList.remove("text-green");
-        showMessage.click();
-        return false;
+    if (that.id === "contact-form-desktop") {
+        console.log("hello");
+        let fName = document.forms["ideasFormDesktop"]["firstNameDesktop"].value;
+        let lName = document.forms["ideasFormDesktop"]["lastNameDesktop"].value;
+        let email = document.forms["ideasFormDesktop"]["emailAddressDesktop"].value;
+        let suggestion = document.forms["ideasFormDesktop"]["suggestionDesktop"].value;
+        let errormsg = document.getElementById("formErrorMessage");
+        let showMessage = document.getElementById("navigateToFooter");
+        let letters = /^[A-Za-z]+$/;
+        let emailContent = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (fName == "") {
+            errormsg.innerHTML = "**Please enter your first name**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else if (lName == "") {        
+            errormsg.innerHTML = "**Please enter your last name**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else if (!fName.match(letters) || !lName.match(letters)) {        
+            errormsg.innerHTML = "**The name fields should only contain characters A-Z**";
+            showMessage.click();
+            return false;
+        } else if (email == "") {
+            errormsg.innerHTML = "**Please enter your email address**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else if (!email.match(emailContent)) {
+            errormsg.innerHTML = "**Please enter a valid email address**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else if (suggestion == "") {
+            errormsg.innerHTML = "**Please enter your message**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else {
+            sendMailDesktop(that);
+            errormsg.innerHTML = `Thanks for getting in touch!<br>We'll get back to you soon <i class="far fa-smile"></i>`
+            errormsg.classList.remove("text-red");
+            errormsg.classList.add("text-green");
+            showMessage.click();
+            return false;
+        }   
+    } else {
+        console.log("hello mobile");
+        let fName = document.forms["ideasForm"]["firstName"].value;
+        let lName = document.forms["ideasForm"]["lastName"].value;
+        let email = document.forms["ideasForm"]["emailAddress"].value;
+        let suggestion = document.forms["ideasForm"]["suggestion"].value;
+        let errormsg = document.getElementById("formErrorMessage");
+        let showMessage = document.getElementById("navigateToFooter");
+        let letters = /^[A-Za-z]+$/;
+        let emailContent = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (fName == "") {
+            errormsg.innerHTML = "**Please enter your first name**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else if (lName == "") {        
+            errormsg.innerHTML = "**Please enter your last name**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else if (!fName.match(letters) || !lName.match(letters)) {        
+            errormsg.innerHTML = "**The name fields should only contain characters A-Z**";
+            showMessage.click();
+            return false;
+        } else if (email == "") {
+            errormsg.innerHTML = "**Please enter your email address**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else if (!email.match(emailContent)) {
+            errormsg.innerHTML = "**Please enter a valid email address**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else if (suggestion == "") {
+            errormsg.innerHTML = "**Please enter your message**";
+            errormsg.classList.add("text-red");
+            errormsg.classList.remove("text-green");
+            showMessage.click();
+            return false;
+        } else {
+            sendMailMobile(that);
+            errormsg.innerHTML = `Thanks for getting in touch!<br>We'll get back to you soon <i class="far fa-smile"></i>`
+            errormsg.classList.remove("text-red");
+            errormsg.classList.add("text-green");
+            showMessage.click();
+            return false;
+        }
     }
 }
